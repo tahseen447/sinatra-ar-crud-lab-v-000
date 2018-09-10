b948b4ffe5b909a5c56a43e4a6b80e45dfab1ddd
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-10 19:44:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post '/posts' do
     @post = Post.create(params)
-    @posts = Post.all
-    erb :index
+    redirect to erb :index
   end
 
   get '/posts' do
